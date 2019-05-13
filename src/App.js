5c0c56abef604be120e939e438e8760163dde001
Elshad <safarov.elshad007@gmail.com>
2019-05-14 00:09:30 +0500

    Changed componentDidMount method

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import dummyData from './components/dummy-data';
 import MessageList from './components/MessageList';
+import Chatkit from "@pusher/chatkit";
 import '@atlaskit/css-reset';
 import './styles/index.css';
 
 const instanceLocator = "v1:us1:abe8898e-8cfc-405b-899f-b428533baa64";
-const testToken = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/abe8898e-8cfc-405b-899f-b428533baa64/token";
+const testToken = "https://us1.pusherplatform.io/services/chatkit-token-provider/v1/abe8898e-8cfc-405b-899f-b428533baa64/token";
 const username = "Perborgen";
 const roomId = 21148281;
 
@@ -17,14 +18,30 @@ class App extends React.Component {
             messages: dummyData
         };
     }
-    componentDidMount() { 
+    componentDidMount() {
         const chatManager = new Chatkit.ChatManager({
             instanceLocator: instanceLocator,
-            userId: username,
+            userId: 'janedoe',
             tokenProvider: new Chatkit.TokenProvider({
                 url: testToken
             })
-        });
+        })
+
+        chatManager.connect()
+            .then(currentUser => {
+                this.currentUser = currentUser
+                this.currentUser.subscribeToRoom({
+                    roomId: roomId,
+                    hooks: {
+                        onNewMessage: message => {
+
+                            this.setState({
+                                messages: [...this.state.messages, message]
+                            })
+                        }
+                    }
+                })
+            })
     }
     render() {
         console.log(this.state.messages);
