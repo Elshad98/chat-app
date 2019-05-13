be5c79536ff36eb339d61e549d64adad96e7e064
Elshad <safarov.elshad007@gmail.com>
2019-05-13 22:51:36 +0500

    Added componentDidMount method in App class

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import MessageList from './components/MessageList';
 import '@atlaskit/css-reset';
 import './styles/index.css';
 
+const instanceLocator = "v1:us1:abe8898e-8cfc-405b-899f-b428533baa64";
+const testToken = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/abe8898e-8cfc-405b-899f-b428533baa64/token";
+const username = "Perborgen";
+const roomId = 21148281;
+
 class App extends React.Component {
     constructor(props) {
         super(props);
@@ -11,6 +16,15 @@ class App extends React.Component {
         this.state = {
             messages: dummyData
         };
+    }
+    componentDidMount() { 
+        const chatManager = new Chatkit.ChatManager({
+            instanceLocator: instanceLocator,
+            userId: username,
+            tokenProvider: new Chatkit.TokenProvider({
+                url: testToken
+            })
+        });
     }
     render() {
         console.log(this.state.messages);
--- src/index.js
+++ src/index.js
@@ -3,11 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const instanceLocator = "v1:us1:abe8898e-8cfc-405b-899f-b428533baa64";
-const testToken = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/abe8898e-8cfc-405b-899f-b428533baa64/token";
-const username = "Perborgen";
-const roomId = 21148281;
-
 ReactDOM.render(<App />, document.querySelector('#root'));
 
 serviceWorker.unregister();
