306e7009eadcea673026cdccdd99f0dd4d5ea0ca
Elshad <safarov.elshad007@gmail.com>
2019-05-13 22:20:07 +0500

    Fetching API-keys from Chatkit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const instanceLocator = "v1:us1:abe8898e-8cfc-405b-899f-b428533baa64";
+const testToken = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/abe8898e-8cfc-405b-899f-b428533baa64/token";
+const username = "Perborgen";
+const roomId = 21148281;
+
+ReactDOM.render(<App />, document.querySelector('#root'));
 
 serviceWorker.unregister();
