bb9bc508dfdd6147d24a064523b43c6e5d656787
Elshad <safarov.elshad007@gmail.com>
2019-05-14 21:28:52 +0500

    Added handleChange method in SendMessageFrom class

--- src/components/SendMessageForm.js
+++ src/components/SendMessageForm.js
@@ -1,11 +1,27 @@
 import React from 'react';
 
 class SendMessageForm extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+            message: ''
+        };
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(evt){
+        this.setState({
+            message: evt.target.value
+        });
+    }
+
     render(){
         return (
             <form className="send-message-form">
                 <input 
-                    placeholder="SendMessageForm"
+                    onChange={this.handleChange}
+                    value={this.state.message}
+                    placeholder="Type your message and hit ENTER"
                     type="text"
                 />
             </form>
