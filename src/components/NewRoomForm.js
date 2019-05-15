e73fd8d561ab22476155dddc8e824f824ed4c408
Elshad <safarov.elshad007@gmail.com>
2019-05-15 19:39:03 +0500

    Added handleChange method in NewRoomForm class

--- src/components/NewRoomForm.js
+++ src/components/NewRoomForm.js
@@ -1,12 +1,28 @@
 import React from 'react';
 
 class NewRoomForm extends React.Component{
+    constructor(){
+        super();
+
+        this.state = {
+            roomName: ''
+        };
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(evt){
+        this.setState({
+            roomName: evt.target.value
+        });
+    }
+
     render(){
         return (
             <div className="new-room-form">
                 <form>
                     <input 
                         onChange={this.handleChange}
+                        value={this.state.roomName}
                         type="text" 
                         placeholder="NewRoomForm" 
                         required />
